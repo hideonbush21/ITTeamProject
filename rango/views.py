793e6968ae36bdcceaf4b3b7176578f0bc32a64a
zouqiaoxuan <1571559532@qq.com>
2021-08-04 21:00:17 +0800

    1.update base.html,login.html,register.html 2.add login.css 3.install bootstrap3 4.add user.png into static/images
    author:Qiaoxuan Zou

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,16 @@ from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from datetime import datetime
+from django import forms
+from django.forms import widgets
+
+
+class UserForm(forms.Form):
+    username = forms.CharField(min_length=4, label='Username', widget=widgets.TextInput(attrs={"class": "form-input  {% if user_form.username.errors.0 %}has-error{% endif %}"})) # 必须用required
+    password = forms.CharField(min_length=6, label='Password', widget=widgets.TextInput(attrs={"class": "form-input"}))
+    # password_confirm = forms.CharField(min_length=4, label='Password Confirm')
+    email = forms.EmailField(label='Email', widget=widgets.TextInput(attrs={"class": "form-input"}))
+    # phone = forms.CharField(label='Phone')
 
 
 def index(request):
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'bootstrap3'
 ]
 
 MIDDLEWARE = [
