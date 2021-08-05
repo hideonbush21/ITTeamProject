33cc067e3f13b5e38f11cf2675b0d9fb9ec8398d
“yuyangyyu” <2771342603@qq.com>
2021-08-05 22:30:23 +0100

    a

--- populate_rango.py
+++ populate_rango.py
@@ -16,36 +16,40 @@ def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/',
-         'views': 21},
+         'views': 21,
+         'rank': 1},
         {'title':'How to Think like a Computer Scientist',
          'url':'http://www.greenteapress.com/thinkpython/',
-         'views': 19},
+         'views': 19,
+         'rank': 2},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/',
-         'views': 32} ]
+         'views': 32,
+         'rank': 3}]
 
-    django_pages = [
-        {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
-         'views': 36},
-        {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/',
-         'views': 42},
-        {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/',
-         'views': 48} ]
-
-    other_pages = [
-        {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/',
-         'views': 64},
-        {'title':'Flask',
-         'url':'http://flask.pocoo.org',
-         'views': 72} ]
+    # django_pages = [
+    #     {'title':'Official Django Tutorial',
+    #      'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+    #      'views': 36},
+    #     {'title':'Django Rocks',
+    #      'url':'http://www.djangorocks.com/',
+    #      'views': 42},
+    #     {'title':'How to Tango with Django',
+    #      'url':'http://www.tangowithdjango.com/',
+    #      'views': 48} ]
+    #
+    # other_pages = [
+    #     {'title':'Bottle',
+    #      'url':'http://bottlepy.org/docs/dev/',
+    #      'views': 64},
+    #     {'title':'Flask',
+    #      'url':'http://flask.pocoo.org',
+    #      'views': 72} ]
 
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
-            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+            # 'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            # 'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
+            }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -55,19 +59,27 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'], views=p['views'])
+            add_page(c, p['title'], p['url'], rank=p['rank'], views=p['views'])
 
     # Print out the categories we have added.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, rank=0, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
+    p.rank=rank
     p.save()
     return p
+
+# def add_page(cat, title, url, views=0):
+#     p = Page.objects.get_or_create(category=cat, title=title)[0]
+#     p.url=url
+#     p.views=views
+#     p.save()
+#     return p
 
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
